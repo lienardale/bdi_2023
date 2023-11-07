ec43276e26f42d3b834d67f0242abebfca389d82
lienardale <alex59.facebook@laposte.net>
2023-11-07 23:59:26 +0100

    fix: change layout to bdi

--- app/home/(overview)/page.tsx
+++ app/home/(overview)/page.tsx
@@ -11,14 +11,14 @@ import {
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Dashboard',
+  title: 'Home',
 };
 
 export default async function Page() {
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard
+        Home
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardSkeleton/>}>
@@ -26,12 +26,12 @@ export default async function Page() {
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <Suspense fallback={<RevenueChartSkeleton />}>
+        {/* <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
         <Suspense fallback={<LatestInvoicesSkeleton/>}>
           <LatestInvoices />
-        </Suspense>
+        </Suspense> */}
       </div>
     </main>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,10 @@ import { Metadata } from 'next';
  
 export const metadata: Metadata = {
   title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    template: '%s | La BDI',
+    default: 'La BDI',
   },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
+  description: 'The official BDI website.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
 
